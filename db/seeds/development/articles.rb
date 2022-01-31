44b5cdc60d9b95dbc779c63796a0f9926aed1754
99p <poccori@gmail.com>
2022-01-31 22:38:21 +0900

    finish chapter9. add pagination

--- db/seeds/development/articles.rb
+++ db/seeds/development/articles.rb
@@ -3,12 +3,12 @@ body = "MorningGloryが4対2でSunflowerに勝利。\n\n" +
   "９回表、ランナー一、二塁で２番田中の二塁打で２点を挙げ、ダメを押しました。\n\n" +
   "投げては初先発の山本が８回を２失点に抑え、伊藤、中村と繋いで逃げ切りました。"
 
-0.upto(9) do |idx|
+0.upto(29) do |idx|
   Article.create(
-    title: "練習試合の結果 #{idx}",
-    body: body,
-    released_at: 8.days.ago.advance(days: idx),
-    expired_at: 2.days.ago.advance(days: idx),
-    member_only: (idx % 3 == 0)
+    title: "Article#{idx+10}",
+    body: "blah,  blah, blah..",
+    released_at: 100.days.ago.advance(days: idx),
+    expired_at: nil,
+    member_only: false,
   )
 end
